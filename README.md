ef07f79c335436ec177cd91a4838f20992e39d42
Jason <sj4axao@163.com>
2018-06-27 10:43:30 +0800

    source cord

---
+++ README.md
@@ -0,0 +1,35 @@
+﻿# README
+
+`kuduUtil`
+
+---
+**pom：**
+```xml
+<dependency>
+    <groupId>com.fulihui</groupId>
+    <artifactId>kudu-spring-boot-starter</artifactId>
+</dependency>
+```
+**必要配置:**
+```yml
+kudu:
+  kuduAddress: 192.168.1.131,192.168.1.132,192.168.1.111  hostlist[]
+  workerId: 0 节点id [0,1023] after 1.1.0
+```
+**注入:**
+```java
+@Autowired
+private KuduUtil kuduUtil;
+// 当然可以直接注入 KuduClient 和 KuduSession 来使用原生 api
+```
+
+---
+versions
+-
+- 1.0.0-SNAPSHOT 1.0快照版，提供基本增删改查功能
+- 1.1.0-SNAPSHOT 
+> + 增加Long类型的ID生成器
+> + 增加兼容多 DB 的重载方法，默认仍然是 ***
+> + 增加 upsert 方法
+
+- 2.0.0-SNAPSHOT 准备分享
---
+++ src/main/java/com/fulihui/stater/kudu/helper/IdGenerator.java
@@ -0,0 +1,70 @@
+package com.fulihui.stater.kudu.helper;
+
+
+/**
+ * @author: Levon
+ * @version: v 0.1 2018-03-12 15:33
+ */
+public class IdGenerator {
+
+    //Mon Mar 12 15:41:17 CST 2018
+    private final static long twepoch = 1520840477347L;             //日期起始点
+
+    private final long workerId ;
+    private long sequence = 0L;
+    private long lastTimestamp = -1L;
+
+    private final static long workerIdBits = 10L;                        //机器ID占用10bits
+    private final static long sequenceBits = 12L;                        //序列占用12bits
+
+    public final static long maxWorkerId = -1L ^ -1L << workerIdBits;  //机器ID 最大值
+
+    private final static long timestampLeftShift = sequenceBits + workerIdBits;//时间偏移位
+    private final static long workerIdShift = sequenceBits;               //机器ID偏移位
+
+    public final static long sequenceMask = -1L ^ -1L << sequenceBits;  //序列掩码
+
+
+    public IdGenerator(final long workerId) {
+        super();
+        if (workerId > maxWorkerId || workerId < 0) {
+            throw new IllegalArgumentException(String.format("worker Id can't be greater than %d or less than 0", maxWorkerId));
+        }
+        this.workerId = workerId;
+    }
+
+    public synchronized long nextId() {
+        long timestamp = this.timeGen();
+        if (this.lastTimestamp == timestamp) {
+            this.sequence = (this.sequence + 1) & sequenceMask;
+            if (this.sequence == 0) {
+                timestamp = this.tilNextMillis(this.lastTimestamp);
+            }
+        } else {
+            this.sequence = 0;
+        }
+        if (timestamp < this.lastTimestamp) {
+            try {
+                throw new Exception(String.format("Clock moved backwards.  Refusing to generate id for %d milliseconds", this.lastTimestamp - timestamp));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        this.lastTimestamp = timestamp;
+        long nextId = ((timestamp - twepoch << timestampLeftShift)) | (this.workerId << workerIdShift) | (this.sequence);
+        return nextId;
+    }
+
+    private long tilNextMillis(final long lastTimestamp) {
+        long timestamp = this.timeGen();
+        while (timestamp <= lastTimestamp) {
+            timestamp = this.timeGen();
+        }
+        return timestamp;
+    }
+
+    private long timeGen() {
+        return System.currentTimeMillis();
+    }
+
+}

a89daea3e0bf1818200db3efdf413b55907d7cbd
huanqingdong <609139528@qq.com>
2019-02-11 15:17:54 +0800

    移除无效代码

--- src/main/java/org/sj4axao/stater/kudu/config/KuduClientAutoConfiguration.java
+++ src/main/java/org/sj4axao/stater/kudu/config/KuduClientAutoConfiguration.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,7 +26,7 @@ import java.util.List;
  * @description:
  */
 @Configuration
-//@EnableConfigurationProperties(KuduProperties.class)
+@EnableConfigurationProperties(KuduProperties.class)
 public class KuduClientAutoConfiguration {
 
     private static final Logger logger = LoggerFactory.getLogger(KuduClientAutoConfiguration.class);
@@ -41,45 +42,10 @@ public class KuduClientAutoConfiguration {
     public KuduClient kuduClient(@Qualifier("kuduProperties") KuduProperties kuduProperties) throws IOException, InterruptedException {
         List<String> masterAddr = Arrays.asList(kuduProperties.getKuduAddress().split(","));
         logger.info("kuduClient实例化,servers:{}", masterAddr);
-        KuduClient kuduClient;
-        kuduClient = new KuduClient.KuduClientBuilder(masterAddr).build();
-        /*if (StringUtils.isBlank(kuduProperties.getKerberosKeytab())) {
-            logger.info("未配置kerberos...");
-            kuduClient = new KuduClient.KuduClientBuilder(masterAddr).build();
-        } else {
-            logger.info("配置了kerberos...");
-            initKerberosENV(kuduProperties);
-            kuduClient = UserGroupInformation.getLoginUser().doAs(
-                    (PrivilegedExceptionAction<KuduClient>) () -> new KuduClient.KuduClientBuilder(masterAddr).build()
-            );
-        }*/
+        KuduClient kuduClient = new KuduClient.KuduClientBuilder(masterAddr).build();
         logger.info("kuduClient实例化结束");
         return kuduClient;
     }
-
-    /**
-     * 初始化访问Kerberos访问
-     * 参考:https://mp.weixin.qq.com/s?__biz=MzI4OTY3MTUyNg==&mid=2247493634&idx=1&sn=e0c28169586a6afdb4f0c5e753473638&chksm=ec29380bdb5eb11d2e21d40a2be7c60602b2c30af11c32321e4bd505d4ee99e1ec0207965442&scene=21#wechat_redirect
-     */
-    /*public static void initKerberosENV(KuduProperties kuduProperties) {
-        try {
-            System.setProperty("javax.security.auth.useSubjectCredsOnly", "false");
-            if (kuduProperties.isKerberosDebug()) {
-                System.setProperty("sun.security.krb5.debug", "true");
-            }
-
-            org.apache.hadoop.conf.Configuration configuration = new org.apache.hadoop.conf.Configuration();
-            //configuration.addResource(KuduClientAutoConfiguration.class.getClass().getResourceAsStream("/hadoop-conf/core-site.xml"));
-            //configuration.addResource(KuduClientAutoConfiguration.class.getClass().getResourceAsStream("/hadoop-conf/hdfs-site.xml"));
-            configuration.set("hadoop.security.authentication", "kerberos");
-            UserGroupInformation.setConfiguration(configuration);
-
-            UserGroupInformation.loginUserFromKeytab(kuduProperties.getKerberosPrincipal(), kuduProperties.getKerberosKeytab());
-            logger.info("初始化kerberos环境完成,用户:{}" , UserGroupInformation.getCurrentUser());
-        } catch (Exception e) {
-            logger.error("初始化kerberos环境失败..", e);
-        }
-    }*/
 
     @Bean(destroyMethod = "close")
     @ConditionalOnBean(KuduClient.class)
